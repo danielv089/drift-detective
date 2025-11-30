285f8ee6999b5a39e0c2cf5419fd8bc7978e6d53
Daniel Varga <bender@fedora-2.home>
2025-11-30 18:50:15 +0100

    Dataframe snapshot class created

---
+++ src/df_schema.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import json
+from pathlib import Path
+from datetime import datetime
+
+class CSVSchema:
+
+    def __init__(self, df: pd.DataFrame = None, filepath: str = None, table_name: str = None):
+        if df is not None:
+            self.df = df
+            self.filepath = filepath if filepath else "unknown"
+            self.name = table_name if table_name else "unknown"
+        elif filepath is not None:
+            self.df = pd.read_csv(filepath)
+            self.filepath = filepath
+            self.name = table_name if table_name else Path(filepath).name
+        else:
+            raise ValueError("You must provide either a DataFrame or a CSV file path.")
+        
+    def check_columns(self):
+        return list(self.df.columns)
+    
+    def num_columns(self):
+        return len(self.df.columns)
+    
+    def num_rows(self):
+        return len(self.df)
+
+    def check_schema(self):
+        schema = {col: str(dtype) for col, dtype in self.df.dtypes.items()}
+        return schema
+    
+    def schema_snapshot(self,):
+            snapshot = {
+                "table_name": self.name,
+                "filepath": self.filepath,
+                "snapshot_time": datetime.now().isoformat(),
+                "column_count": self.num_columns(),
+                "row_count": self.num_rows(),
+                "schema": self.check_schema(),
+                }
+            
+            json_snapshot=json.dumps(snapshot, indent=4)
+            return json_snapshot
+
