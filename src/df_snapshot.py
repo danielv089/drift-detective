b96f0ecc69cd85402572dc4bf86f1d9d6dc2f988
Daniel Varga <bender@fedora-2.home>
2025-12-01 21:32:09 +0100

    Added version numbering

--- src/df_snapshot.py
+++ src/df_snapshot.py
@@ -3,7 +3,7 @@ import json
 from pathlib import Path
 from datetime import datetime
 
-class CSVSchema:
+class DfSnapshot:
 
     def __init__(self, df: pd.DataFrame = None, filepath: str = None, table_name: str = None):
         if df is not None:
@@ -16,6 +16,7 @@ class CSVSchema:
             self.name = table_name if table_name else Path(filepath).name
         else:
             raise ValueError("You must provide either a DataFrame or a CSV file path.")
+        self.version=1
         
     def check_columns(self):
         return list(self.df.columns)
@@ -30,16 +31,16 @@ class CSVSchema:
         schema = {col: str(dtype) for col, dtype in self.df.dtypes.items()}
         return schema
     
-    def schema_snapshot(self,):
+    def snapshot(self):
             snapshot = {
                 "table_name": self.name,
                 "filepath": self.filepath,
                 "snapshot_time": datetime.now().isoformat(),
+                "version": self.version,
                 "column_count": self.num_columns(),
                 "row_count": self.num_rows(),
                 "schema": self.check_schema(),
                 }
             
-            json_snapshot=json.dumps(snapshot, indent=4)
-            return json_snapshot
+            return json.dumps(snapshot, indent=4)
 
