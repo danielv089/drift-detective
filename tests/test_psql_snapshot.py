1ff70778c642e1c765daeed7b4fe65aac4290eda
Daniel Varga <varga.dani6@gmail.com>
2026-01-13 21:19:16 +0100

    Gitignore update

--- tests/test_psql_snapshot.py
+++ tests/test_psql_snapshot.py
@@ -3,5 +3,5 @@ from pathlib import Path
 
 import pytest
 
-from detective_tools.psql_snapshot import PsqlSnapshot
+from detective_tools.postgres_snapshot import PsqlSnapshot
 from detective_tools.schema_versioning import SchemaVersioning
