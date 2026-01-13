b0dd36784c8e7ce3a35071b04eb98e079c174ee9
Daniel Varga <varga.dani6@gmail.com>
2026-01-13 21:12:22 +0100

    Corrected typos and add sqlalchemy as dependecy

--- src/detective_tools/__init__.py
+++ src/detective_tools/__init__.py
@@ -1,12 +1,12 @@
 from .df_snapshot import DfSnapshot
-from .schema_versioning import DfSchemaVersioning
+from .schema_versioning import SchemaVersioning
 from .snapshot_history import SnapshotHistory
 from .snapshot_diff import SnapshotDiff
 from .report import SchemaReport
 
 __all__ = [
     "DfSnapshot",
-    "DfSchemaVersioning",
+    "SchemaVersioning",
     "SnapshotHistory",
     "SnapshotDiff",
     "SchemaReport",
